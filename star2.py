aa87054a6511b49702fe17e01399bc749bb3008b
piyush054 <72505194+piyush054@users.noreply.github.com>
2021-10-16 21:19:27 +0530

    Update star2.py

--- star2.py
+++ star2.py
@@ -1,6 +1,5 @@
-while True:
-    n = int(input('Enter te no. of lines: '))
-    for i in range(n+1):
+n = int(input('Enter te no. of lines: '))
+for i in range(n+1):
         for j in range(n+1):
             if j <= i and j >= (n-i) or j >= i and j <= (n-i) and j <= n:
                 print("* ", end = "")
