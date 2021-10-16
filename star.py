e4615eb27201e9131d5db8a04f04bf72f9fc34d4
piyush054 <72505194+piyush054@users.noreply.github.com>
2021-10-16 21:18:54 +0530

    Update star.py

--- star.py
+++ star.py
@@ -1,6 +1,5 @@
-while True:
-    a= int(input('Enter the no. of lines : '))
-    def pattern(n):
+a= int(input('Enter the no. of lines : '))
+def pattern(n):
           k = 2 * n - 2
           for i in range(0,n):
                for j in range(0,k):
